3115ad3510b66933b6856cc4935b2fb9c0ba99fe
Juan-Diego07 <juandiegoprendas@gmail.com>
2022-05-31 18:13:17 -0600

    feature JPR-B96105 Pokemon Trainer CRUD done

--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,8 @@
 class PokemonTrainer < ApplicationRecord
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :id_number, presence: true, uniqueness: true
+
+    enum level: {high: 0, medium: 1, low: 2}
 end
