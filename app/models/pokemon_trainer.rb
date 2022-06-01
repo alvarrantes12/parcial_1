8353d83c008f6f628fc0e15b80acd22d71ee49b6
iOrlandoA <orlando.aguilar@ucrso.info>
2022-05-31 18:33:38 -0600

    Pokemon trainer CRUD done

--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,7 @@
 class PokemonTrainer < ApplicationRecord
+    validates :first_name, presence: true, length: {minimun: 1, maximum:10} 
+    validates :last_name, presence: true, length: {minimun: 1, maximum:15} 
+    validates :id_number, uniqueness: true, presence: true, length: {minimun: 8, maximum:10} 
+
+    enum level: { low: 0, medium: 1, high: 2 }
 end
--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,10 @@
 require_relative "boot"
 
 require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Lenguajes1
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
