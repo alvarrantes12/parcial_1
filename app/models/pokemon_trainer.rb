340e5a583c53438be1d1d3fc01d7309181049341
roibingdoelizondo99@gmail.com <roibingdoelizondo99>
2022-05-31 18:16:43 -0600

    Pokemon trainer CRUD done

--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,8 @@
 class PokemonTrainer < ApplicationRecord
+
+    validates :first_name, presence: true, length: { maximum: 10}
+    validates :last_name, presence: true, length: { maximum: 15}
+    validates :id_number, uniqueness: true, presence: true, length: { in: 8..10}
+    
+    enum level: {high: 0, medium: 1, low: 2}
 end
