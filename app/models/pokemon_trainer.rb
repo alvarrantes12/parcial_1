738e852fa664b87d9f6fbc2e6c7d101f81b6c488
KendSoto02 <sotofajardo05@gmail.com>
2022-05-31 22:40:40 -0600

    Pokemon trainer CRUD done

--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,7 @@
 class PokemonTrainer < ApplicationRecord
+	validates :first_name, presence: true, length: { maximum: 10 }
+	validates :last_name, presence: true, length: { maximum: 15 }
+	validates :id_number, uniqueness: true, presence: true, length: { in: 8..10 }
+
+	enum level: { higth: 0, medium: 1, low: 2 }
 end
