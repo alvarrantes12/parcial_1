c3e8c6851bdc6a518e16fe7879509fe2d0ccc2ae
Daniel-ChR04 <daniel.chato@ucrso.info>
2022-05-31 19:34:24 -0600

    Pokemon trainer CRUD done

--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,14 @@
 class PokemonTrainer < ApplicationRecord
+    validates :first_name, presence: true
+    validates :first_name, length: {maximum: 10}
+
+    validates :last_name, presence: true
+    validates :last_name, length: {maximum: 15}
+    
+    validates :id_number, presence: true
+    validates :id_number, length: {minimum: 8}
+    validates :id_number, length: {maximum: 10}
+    validates :id_number, uniqueness: true
+
+    enum level: { low: 1, medium: 2, high: 3 }
 end
