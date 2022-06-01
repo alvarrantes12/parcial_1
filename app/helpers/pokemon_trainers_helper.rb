22c5ee504fc87d0da3c89240e04efb24dd282da7
IssacBrenes <issacbrenes@gmail.com>
2022-05-31 19:21:43 -0600

    Pokemon trainer CRUD done

--- app/helpers/pokemon_trainers_helper.rb
+++ app/helpers/pokemon_trainers_helper.rb
@@ -1,2 +1,7 @@
 module PokemonTrainersHelper
+    def level_trainer
+        PokemonTrainer.levels.keys.map do |levels|
+            [t("activerecord.attributes.pokemon_trainer.levels.#{levels}"), levels]
+        end
+    end
 end
--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,7 @@
 class PokemonTrainer < ApplicationRecord
+    validates :first_name, presence: true, length: {minimum: 1, maximum: 10}
+    validates :last_name, presence: true, length: {minimum: 1, maximum: 15} 
+    validates :id_number, uniqueness: true, presence: true,length: {minimum: 8, maximum: 10}
+
+    enum level: { Bajo: 0, Medio: 1, Alto:2 }
 end
