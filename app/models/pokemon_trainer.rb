371cf5ec17572b0e5350dbcb747d6f221a7f2632
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2022-05-31 18:56:48 -0600

    Paso 4

--- app/models/pokemon_trainer.rb
+++ app/models/pokemon_trainer.rb
@@ -1,2 +1,5 @@
 class PokemonTrainer < ApplicationRecord
+  validates :first_name, presence: true, length: {maximum: 10}
+  validates :last_name, presence: true, length: {maximum: 15}
+  validates :id_number, presence: true, length: {minimum: 8, maximum: 10}, uniqueness: true
 end
